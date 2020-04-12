26980c00e41593a57381e1455f33157e0d22ff89
Eli <elijvettel@gmail.com>
2020-04-12 14:34:22 -0500

    Add files via upload
    Added voltage reading and ADS1115 ADC integration

--- EMGgraph.py
+++ EMGgraph.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import tmp102
+import time
+import Adafruit_ADS1x15
 
 # Parameters
 x_len = 200         # Number of points to display
@@ -13,25 +14,36 @@ xs = list(range(0, 200))
 ys = [0] * x_len
 ax.set_ylim(y_range)
 
-# Initialize communication with TMP102
-tmp102.init()
+# Create instance of ADS1115 ADC
+adc = Adafruit_ADS1x15.ADS1115()
+
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 1
 
 # Create a blank line. We will update the line in animate
 line, = ax.plot(xs, ys)
 
 # Add labels
-plt.title('TMP102 Temperature over Time')
+plt.title('Millivolts over Time')
 plt.xlabel('Samples')
-plt.ylabel('Temperature (deg C)')
+plt.ylabel('Milivolts')
 
 # This function is called periodically from FuncAnimation
 def animate(i, ys):
 
-    # Read temperature (Celsius) from TMP102
-    temp_c = round(tmp102.read_temp(), 2)
+    # Read voltage from ADC ADS1115
+    voltage = adc.read_adc(1, gain=GAIN)
 
-    # Add y to list
-    ys.append(temp_c)
+    # Add y voltage to list
+    ys.append(voltage)
 
     # Limit y list to set number of items
     ys = ys[-x_len:]
